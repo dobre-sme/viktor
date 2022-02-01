16f8962f193e2ce99f277c5dd9ba5a30e615e8c0
Viktor <pebblevideos@gmail.com>
2022-02-01 15:28:52 +0200

    Update Camping.java

--- Camping.java
+++ Camping.java
@@ -1,11 +1,10 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import static java.util.Comparator.comparingInt;
+import static java.util.stream.Collectors.toMap;
 
+// map sorted but not in reverse order due to lack of knowladge
 public class Camping {
 	public static void main(String[] args) {
 
@@ -13,8 +12,8 @@ public class Camping {
 		String pattern = "(?<person>[A-Z-a-z]+) (?<model>[0-9A-Z-a-z]+) (?<nights>[0-9]+)$";
 		Pattern compiler = Pattern.compile(pattern);
 		
-		Map<String, List<String>> carOwner = new HashMap<String, List<String>>();
-		Map <String, Integer> nightSpent = new HashMap<String, Integer>();
+		Map<String, List<String>> carOwner = new TreeMap<>();
+		Map <String, Integer> nightSpent = new HashMap<>();
 		
 		while(true) {
 			String test = scan.nextLine();
@@ -35,11 +34,21 @@ public class Camping {
 				int oldNights = nightSpent.get(name);
 				nightSpent.put(name, oldNights + nights);
 			}
-		}// fills maps
-		for (Map.Entry<String, List<String>> entry : carOwner.entrySet()) {
+		}
+
+		Map<String, List<String>> sorted = carOwner.entrySet().stream()
+				.sorted(comparingInt(e -> e.getValue().size()))
+				.collect(toMap(
+						Map.Entry::getKey,
+						Map.Entry::getValue,
+						(b, a) -> { throw new AssertionError(); },
+						LinkedHashMap::new
+				));
+
+		for (Map.Entry<String, List<String>> entry : sorted.entrySet()) {
 			System.out.println(entry.getKey() + ": " + entry.getValue().size());
-			for (int j=0;j<carOwner.get(entry.getKey()).size();j++)  System.out.println("***" + entry.getValue().get(j));
-			System.out.println("Total stay: " + nightSpent.get(entry.getKey()) + " nigths");
+			for (int j=0;j<sorted.get(entry.getKey()).size();j++)  System.out.println("***" + entry.getValue().get(j));
+			System.out.println("Total stay: " + nightSpent.get(entry.getKey()) + " nights");
 		} // prints the person, his vehicles and nights spent
 	}
 }
